60aa18e679504253d22da031c88e6eafcd3f80bf
“hinczhang” <hinczhang@whu.edu.cn>
2023-04-26 22:48:20 +0200

    Add all objs and test some scripts.

---
+++ TUMAR/Assets/Codes/testGettingObj.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testGettingObj : MonoBehaviour
+{
+    private GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GameObject.Find("Experiment"); //替换为你的Cube对象的名称
+        if (obj != null)
+        {
+            Vector3 size = GetObjectSize(obj);
+            Debug.Log("Object size: " + size);
+        } else {
+            Debug.Log("Object not found");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GetObjectSize(GameObject obj)
+    {
+        Vector3 objectSize = Vector3.zero;
+        Renderer renderer = obj.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            objectSize = renderer.bounds.size;
+        }
+        return objectSize;
+    }
+}
